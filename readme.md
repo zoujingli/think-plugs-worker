092ba997a6bc5b2583af62f908787f12287e1bd1
邹景立 <zoujingli@qq.com>
2023-03-21 13:30:08 +0800

    修改域名及IP端口处理

--- readme.md
+++ readme.md
@@ -7,14 +7,14 @@
 [![PHP Version](https://img.shields.io/badge/php-%3E%3D7.1-8892BF.svg)](http://www.php.net)
 [![License](https://poser.pugx.org/zoujingli/think-plugs-worker/license)](https://packagist.org/packages/zoujingli/think-plugs-worker)
 
-基于 **Workerman** 的 **HttpServer** 服务插件 ，用于快速构建内存服务环境！
+基于 **Workerman** 的 **HttpServer** 服务插件 ，构建内存方式的 Web 服务！
 
 代码主仓库放在 **Gitee**，**Github** 仅为镜像仓库用于发布 **Composer** 包。
 
 ### 安装插件
 
 ```shell
-### 安装前建议更新全部组件
+### 安装前建议尝试更新所有组件
 composer update --optimize-autoloader
 
 ### 注意，插件仅支持在 ThinkAdmin v6.1 中使用
@@ -44,14 +44,14 @@ composer remove zoujingli/think-plugs-worker
     ],
     // 监控文件变更重载
     'files'   => [
-        // 监控检测间隔（单位秒，零不监控）
+        // 监控检测间隔（单位秒，零表示不监控）
         'time' => 3,
         // 文件监控目录（默认监控 app 目录）
         'path' => [],
     ],
     // 监控内存超限重载
     'memory'  => [
-        // 监控检测间隔（单位秒，零不监控）
+        // 监控检测间隔（单位秒，零表示不监控）
         'time'  => 60,
         // 限制内存大小（可选单位有 G M K ）
         'limit' => '1G'
--- src/support/Request.php
+++ src/support/Request.php
@@ -29,6 +29,7 @@ use Workerman\Worker;
  */
 class Request extends \think\Request
 {
+
     public function withWorkerRequest(TcpConnection $connection, WorkerRequest $request): Request
     {
         $this->get = $request->get();
@@ -39,35 +40,44 @@ class Request extends \think\Request
         $this->method = $request->method();
         $this->request = $this->post + $this->get;
         $this->pathinfo = ltrim($request->path(), '\\/');
-        // 兼容代理模式
-        $this->host = $this->header['x-host'] ?? ($this->header['remote-host'] ?? $request->host());
+
+        // 请求真实IP
         $this->realIP = $this->header['x-real-ip'] ?? ($this->header['x-forwarded-for'] ?? $connection->getRemoteIp());
+
+        // 请求地址域名及端口处理
+        $this->host = $this->header['x-host'] ?? ($this->header['x-requested-host'] ?? ($this->header['remote-host'] ?? $request->host()));
+        $port = intval($this->header['x-requested-port'] ?? (strpos($this->host, ':') !== false ? explode(':', $this->host)[1] : $connection->getRemoteIp()));
+        if (strpos($this->host, ':') !== false && in_array($port, [80, 443])) $this->host = strstr($this->host, ':', true);
+
         // 服务变量替换
         return $this->withInput($request->rawBody())->withServer(array_filter([
-            'HTTP_HOST'             => $this->host,
-            'PATH_INFO'             => $this->pathinfo,
-            'REQUEST_URI'           => $request->uri(),
-            'SERVER_NAME'           => $request->host(true),
-            'SERVER_ADDR'           => $connection->getLocalIp(),
-            'SERVER_PORT'           => $connection->getLocalPort(),
-            'REMOTE_ADDR'           => $this->realIP,
-            'REMOTE_PORT'           => $connection->getRemotePort(),
-            'QUERY_STRING'          => $request->queryString(),
-            'REQUEST_METHOD'        => $request->method(),
-            'HTTP_X_PJAX'           => $this->header['x-pjax'] ?? null,
-            'HTTP_X_REQUESTED_WITH' => $this->header['x-requested-with'] ?? null,
-            'HTTP_X_FORWARDED_PORT' => $this->header['x-forwarded-port'] ?? null,
-            'HTTP_ACCEPT'           => $this->header['accept'] ?? null,
-            'HTTP_ACCEPT_ENCODING'  => $this->header['accept-encoding'] ?? null,
-            'HTTP_ACCEPT_LANGUAGE'  => $this->header['accept-language'] ?? null,
-            'HTTP_USER_AGENT'       => $this->header['user-agent'] ?? null,
-            'HTTP_COOKIE'           => $this->header['cookie'] ?? null,
-            'HTTP_CACHE_CONTROL'    => $this->header['cache-control'] ?? null,
-            'HTTP_PRAGMA'           => $this->header['pragma'] ?? null,
-            'SERVER_SOFTWARE'       => 'Server/' . Worker::VERSION,
-            'REQUEST_TIME'          => time(),
-            'REQUEST_TIME_FLOAT'    => microtime(true),
-            'SERVER_PROTOCOL'       => $this->header['x-scheme'] ?? null
+            'HTTP_HOST'              => $this->host,
+            'PATH_INFO'              => $this->pathinfo,
+            'REQUEST_URI'            => $request->uri(),
+            'SERVER_NAME'            => $request->host(true),
+            'SERVER_ADDR'            => $connection->getLocalIp(),
+            'SERVER_PORT'            => $port,
+            'REMOTE_ADDR'            => $this->realIP,
+            'REMOTE_PORT'            => $connection->getRemotePort(),
+            'QUERY_STRING'           => $request->queryString(),
+            'REQUEST_METHOD'         => $request->method(),
+            'REQUEST_SCHEME'         => $this->header['x-scheme'] ?? null,
+            'HTTP_X_PJAX'            => $this->header['x-pjax'] ?? null,
+            'HTTP_X_FORWARDED_HOST'  => $this->header['x-forwarded-host'] ?? null,
+            'HTTP_X_REQUESTED_WITH'  => $this->header['x-forwarded-with'] ?? null,
+            'HTTP_X_FORWARDED_PORT'  => $this->header['x-requested-port'] ?? null,
+            'HTTP_X_FORWARDED_PROTO' => $this->header['x-forwarded-proto'] ?? null,
+            'HTTP_ACCEPT'            => $this->header['accept'] ?? null,
+            'HTTP_ACCEPT_ENCODING'   => $this->header['accept-encoding'] ?? null,
+            'HTTP_ACCEPT_LANGUAGE'   => $this->header['accept-language'] ?? null,
+            'HTTP_USER_AGENT'        => $this->header['user-agent'] ?? null,
+            'HTTP_COOKIE'            => $this->header['cookie'] ?? null,
+            'HTTP_CACHE_CONTROL'     => $this->header['cache-control'] ?? null,
+            'HTTP_PRAGMA'            => $this->header['pragma'] ?? null,
+            'SERVER_PROTOCOL'        => $this->header['x-scheme'] ?? ($port === 443 ? 'https' : 'http'),
+            'SERVER_SOFTWARE'        => 'Server/' . Worker::VERSION,
+            'REQUEST_TIME'           => time(),
+            'REQUEST_TIME_FLOAT'     => microtime(true),
         ]));
     }
 }
